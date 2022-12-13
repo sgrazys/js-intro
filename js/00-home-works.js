ea306a54dc222c0c94d1c1984599fd7dc33b77cf
sgrazys <s.grazys@gmail.com>
2022-12-13 17:14:42 +0200

    wip: home task functions

--- js/00-home-works.js
+++ js/00-home-works.js
@@ -324,3 +324,76 @@ console.log(animalsList);
         console.log('Bandykite kitą kartą');
     }
 
+    console.clear();
+
+// FUNKCIJOS
+    // Parašyti funkcijas, kurios atitinka pateiktus reikalavimus, jei įvykdo reikiamus testus
+        /* 1. Funkcija pavadinimu “tusciaFunkcija”:
+            a. nepriima jokių kintamųjų
+            b. neatlieka jokios vidinės logikos
+            c. gražina boolean tipo reikšmę “false”
+            d. TESTAS:
+            i. console.log( tusciaFunkcija() );
+            ii. rezultatas: false */
+
+
+            function tusciaFunkcija () {
+                return false;
+            };
+
+            console.log(`rezultatas: ${tusciaFunkcija()}`);
+            console.log(tusciaFunkcija());
+
+            console.log('------------');
+
+        /* 2. Funkcija pavadinimu “daugyba”:
+            a. priima du skaičiaus tipo kintamuosius
+            b. atskirame kintamajame įsimena sandaugos reikšmę
+            c. gražina saudaugos rezultatą
+            d. TESTAI:
+                i. console.log( daugyba( skaicius1, skaicius2 ) );
+                ii. console.log( daugyba( skaicius3, skaicius2 ) );
+                iii. console.log( daugyba( skaicius1, skaicius3 ) );
+                iv. rezultatas: teisingos reikšmės; */
+
+            function daugyba (skaicius1, skaicius2) {
+                const result = skaicius1 * skaicius2;
+                return result
+            }
+
+            console.log(daugyba(2, 5));
+            console.log(daugyba(11,5));
+            console.log(daugyba(2, 11));
+
+
+        /* 3. Funkcija pavadinimu “skaitmenuKiekisSkaiciuje”:
+            a. priima vieną kintamąjį
+            b. jei perduotas kintamasis nėra skaičiaus tipo, tai išveda pranešimą “Pateikta netinkamo tipo reikšmė.”
+            c. priešingu atveju, funkcija tęsia darbą
+            d. į atskirą kintamąjį įsimena skaičių sudarančių skaitmenų kiekį
+            e. gražina skaitmenų kiekį
+            f. TESTAI:
+                i. console.log( skaitmenuKiekisSkaiciuje( 5 ) );
+                    1. rezultatas: 1
+                ii. console.log( skaitmenuKiekisSkaiciuje( 781 ) );
+                    1. rezultatas: 3
+                iii. console.log( skaitmenuKiekisSkaiciuje( 37060123456 ) );
+                    1. rezultatas: 11
+                iv. console.log( skaitmenuKiekisSkaiciuje( true ) );
+                    1. rezultatas: “Pateikta netinkamo tipo reikšmė.”
+                v.console.log( skaitmenuKiekisSkaiciuje( “asd” ) );
+                    1. rezultatas: “Pateikta netinkamo tipo reikšmė.”
+                vi.console.log( skaitmenuKiekisSkaiciuje( NaN ) );
+                    1. rezultatas: “Pateikta netinkamo tipo reikšmė.” */
+
+
+                    function skaitmenuKiekisSkaiciuje (digit) {
+                        if (typeof digit !== 'number') {
+                            console.log('Pateikta netinkamo tipo reikšmė');
+                        }
+                            const digitLength = digit.toString().length;
+                            return digitLength;
+                        
+                    }
+
+                    console.log(skaitmenuKiekisSkaiciuje('astuoniolikas'));
