1ddedebdaf6314e57aaa26bd2667ae576537a614
sgrazys <s.grazys@gmail.com>
2022-12-15 17:38:19 +0200

    wip: home task - remain (a for loop)

---
+++ tasks/remains.js
@@ -0,0 +1,14 @@
+/* 
+Suskaičiuoti, kiek nurodytame intervale yra skaičių, kurie dalijasi be liekanos iš 3, 5 ir 7 atskirai:
+a. 0 - 11
+b. 8 - 31
+c. -18 - 18
+
+rezultatą pateikti tokiu formatu:
+
+i. Skaičių intervale tarp 0 ir 11, besidalijančių be liekanos iš 3 yra 4 vienetai.
+ii. Skaičių intervale tarp 0 ir 11, besidalijančių be liekanos iš 5 yra 3 vienetai.
+iii. Skaičių intervale tarp 0 ir 11, besidalijančių be liekanos iš 7 yra 2 vienetai.
+     */
+
+console.clear()
