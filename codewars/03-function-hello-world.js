615ae4e85668ae9cab6185a3b972dc1be44690d5
sgrazys <s.grazys@gmail.com>
2022-12-18 23:22:39 +0200

    codewars: solution

---
+++ codewars/03-function-hello-world.js
@@ -0,0 +1,11 @@
+
+// Make a simple function called greet that returns the most-famous "hello world!".
+
+function greet () {
+    let greeting = 'hello';
+    let subject = 'world';
+    
+    return `${greeting} ${subject}!`
+}
+
+console.log(greet());
