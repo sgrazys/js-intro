132bef4919e7b07c18dbcb03798930ff4c2f281d
sgrazys <s.grazys@gmail.com>
2022-12-20 17:23:56 +0200

    solution

--- codewars/04-sentence-smash.js
+++ codewars/04-sentence-smash.js
@@ -6,3 +6,31 @@ EXAMPLE:
 */
 
 
+console.clear();
+
+let wordArr = [1, 2, 3, 4, 5, 6];
+
+function smash (words) {
+    let sentence = '';
+    
+    if (words.length === 0) {
+        return '';
+    }
+
+    for (let i = 0; i < words.length - 1; i++) {
+        sentence += words[i] + ' ';
+    }
+    sentence += words[words.length - 1];
+    return sentence;
+    
+};
+
+
+// SUPER PAPRASTAS VARIANTAS nudoti .join() masyvo items pavercia i string?
+
+// function smash (words) {
+//     return words.join(' ')
+// };
+
+console.log(smash(wordArr));
+console.log(typeof smash(wordArr)); // dbr type yra string
