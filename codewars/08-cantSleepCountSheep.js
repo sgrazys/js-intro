dec1e07be752e04dfefcc5a3f92817c70d493f07
sgrazys <s.grazys@gmail.com>
2022-12-22 00:34:28 +0200

    removed unnecessary comments and code lines

--- codewars/08-cantSleepCountSheep.js
+++ codewars/08-cantSleepCountSheep.js
@@ -5,43 +5,13 @@ Given a non-negative integer, 3 for example, return a string with a murmur: "1 s
 console.clear();
 
 
-console.log(countSheep(0));
-
-// console.log(countSheep(0));
-// console.log(countSheep(1));
-// console.log(countSheep(2));
-// console.log(countSheep(3));
-
-// console.log(countSheep(0), "");
-// console.log(countSheep(1), "1 sheep...");
-// console.log(countSheep(2), "1 sheep...2 sheep..." );
-// console.log(countSheep(3), "1 sheep...2 sheep...3 sheep...");
+var countSheep = function (num) {
+  let sentence = '';
+  for(let i = 1; i <= num; i++) {
+    sentence += `${i} sheep...`;
+  }
+  return sentence
+};
 
 
-
-
-    // if (num === 0) {
-    //   return "";
-    // }
-
-    // let count = 1;
-    // for (let i = 1; i <= num; i++) {
-    //   console.log(count++);
-    // }
-
-
-
-/*     var countSheep = function (num) {
-        if (num === 0) {
-          return "";
-        }
-
-      let str = "";
-
-      for (let i = 1; i <= num; i++) {
-        `${i} sheep...`;
-        str += `${i} sheep...`;
-      }
-
-      return str;
-    }; */
+console.log(countSheep(2));
