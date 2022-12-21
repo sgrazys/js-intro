b04fcbb165a7b6551bee18df65cdbaaad00124f0
sgrazys <s.grazys@gmail.com>
2022-12-21 11:15:17 +0200

    solution: String to number

--- codewars/06-stringToNumber.js
+++ codewars/06-stringToNumber.js
@@ -10,12 +10,14 @@ console.clear();
 
 const stringToNumber = function (str) {
     
-    Number(str);
-    
-    return null;
+    return Number(str);
 };
 
+console.log(stringToNumber('-1234'));
 console.log(stringToNumber('1234'),'--', 1234);
-console.log(stringToNumber("605"), 605);
-console.log(stringToNumber("1405"),1405);
-console.log(stringToNumber("-7"),  -7);
+console.log(stringToNumber("605"),'--', 605);
+console.log(stringToNumber("1405"),'--',1405);
+console.log(stringToNumber("-7"),'--', -7);
+
+
+// const stringToNumber = (str) => Number(str);
