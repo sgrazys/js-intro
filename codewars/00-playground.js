ae186920470b6f3a109357828d3842d5c257d3c8
sgrazys <s.grazys@gmail.com>
2022-12-22 16:12:12 +0200

    playground

---
+++ codewars/00-playground.js
@@ -0,0 +1,26 @@
+console.clear();
+/* 
+Given an array of integers your solution should find the smallest integer.
+
+For example:
+
+Given [34, 15, 88, 2] your solution will return 2
+Given [34, -345, -1, 100] your solution will return -345
+You can assume, for the purpose of this kata, that the supplied array will not be empty.
+*/
+
+/* findSmallestInt(args) {
+    let minNum = args[0];
+
+    for(let i = 1; i < args.length; i++ ) {
+        if(minNum > args[i]) {
+            minNum = args[i];
+        }
+    }
+
+    return minNum;
+} */
+
+
+
+console.clear();
