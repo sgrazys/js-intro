2a7526786db9ab5a41d3b39074771d95e89d77ed
sgrazys <s.grazys@gmail.com>
2022-12-26 00:24:23 +0200

    complex ternary

--- js/11-ternary.js
+++ js/11-ternary.js
@@ -51,3 +51,46 @@ const msgWin = false;
 const msg = (msgWin ? 'Sveikinu' : 'Nenuleisk ranku')
 
 console.log(msg);
+
+console.log('------COMPLEX TERNARY----------');
+// COMPLEX TERNARY
+
+let a = 0 < 1 
+    ? 2 
+    : 3; // 2 nes true (0 yra maziau uz 1)
+console.log(a); 
+
+const b = 0 > 1 
+    ? 2 
+    : 3; // 3 nes false (0 ner daugiau uz 1)
+console.log(b); 
+
+const c = 0 === 1 
+    ? 2 
+    : 3; // grazino 3 nes jie yra ne vienodi
+console.log(c); 
+
+const d = 0 < 1  // nestintas if elsas pvz zemiau
+    ? 2 
+    : 3     
+        ? 4 : 5; 
+console.log(d); 
+
+const e = 0 > 1 // ar 0 > 1, ne
+    ? 2 
+    : 3 // kodas soka cia, kadangi cia nestinta ir atrodo kaip naujas ifas, tda vel klausimas ar 3 daugiau uz 1? --> taip, true ir tuomet ispausdina 4
+            ? 4 
+            : 5; 
+console.log(e); 
+
+const f = 0 < 1 
+    ? 2  // nulis maziau uz 1, tad true reiksme tampa 2, todel klausia ar 2 maziau uz 3, reiksme traip
+        ? 3 // todel grazina 3
+        : 4 
+            
+    : 5;
+console.log(f);
+
+const g = 0 > 1 ? 2 ? 3 : 4 :5 ? 6 : 7;
+
+console.log(g);
