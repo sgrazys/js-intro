2d99321022f2f0889ee97764ed2bdc36d34441ef
sgrazys <s.grazys@gmail.com>
2022-12-29 23:59:52 +0200

    playing

--- codewars/00-playground.js
+++ codewars/00-playground.js
@@ -1,196 +1,65 @@
 console.clear();
+
 /* 
-Given an array of integers your solution should find the smallest integer.
 
-For example:
+Given two integer arrays a, b, both of length >= 1, create a program that returns true if the sum of the squares of each element in a is strictly greater than the sum of the cubes of each element in b.
 
-Given [34, 15, 88, 2] your solution will return 2
-Given [34, -345, -1, 100] your solution will return -345
-You can assume, for the purpose of this kata, that the supplied array will not be empty.
-*/
+arrayMadness([4, 5, 6], [1, 2, 3]); // returns true since 4 ** 2 + 5 ** 2 + 6 ** 2 > 1 ** 3 + 2 ** 3 + 3 ** 3
 
-/* findSmallestInt(args) {
-    let minNum = args[0];
+ */
 
-    for(let i = 1; i < args.length; i++ ) {
-        if(minNum > args[i]) {
-            minNum = args[i];
-        }
+function arrayMadness(a, b) {
+    if(!Array.isArray(b)) {
+        return false
     }
 
-    return minNum;
-} */
+    let sumSqr = a.map((n) => n**2).reduce((t, n) => t + n);
+    let sumCubes = b.map((n) => n**3).reduce((t, n) => t + n);
+    
+    return sumSqr > sumCubes ? true : false;
+}
 
-
+console.log(arrayMadness([1, 2, 3]));
 
 console.clear();
 
-/* 
-Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
 
-Exampl
-[true,  true,  true,  false,
-  true,  true,  true,  true ,
-  true,  false, true,  false,
-  true,  false, false, true ,
-  true,  true,  true,  true ,
-  false, false, true,  true]
+/* 
+Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+
+The output should be two capital letters with a dot separating them.
+
+It should look like this:
+
+Sam Harris => S.H
+
+patrick feeney => P.F
+
+
+
 */
 
-// 1 + 1 + 1 + 0 + 1 + 1 + 1 + 1 
+// function abbrevName(name) {
 
-// function countSheeps(arrayOfSheep) {
-    
-//     let count = 0;
-//     for(const sheep of arrayOfSheep) {
-//         if (sheep) {
-//             count += 1;
-        //  console.log('Sheep yra:', sheep);
-//         }
-//     }
-//         return count;
-//     return arrayOfSheep.filter(sheep => sheep === true).length;
+//     let arr = name.split(' ');
+//     return arr[0][0] + '.' + arr[1][0].toUpperCase();
+//     // console.log(name.split(' ').map((name) => name[0].toUpperCase()).join('.'));
+// }
+
+// abbrevName("Sam harris"), "S.H";
+// abbrevName("Patrick Feenan"), "P.F";
+// abbrevName("P Favuzzi")
+
+// let products = ['Obuolys', 'Morka', 'Arbata', 'Bulves', 'Cepelinai']
+
+// for (let i = 0; i < products.length; i++ ) {
+//     console.log (i+1 + ')', products[i] + ' - ')
 // }
 
 
 
-// console.log(
-//   countSheeps([
-//     true,
-//     true,
-//     true,
-//     false,
-//     true,
-//     true,
-//     true,
-//     true,
-//     true,
-//     false,
-//     true,
-//     false,
-//     true,
-//     false,
-//     false,
-//     true,
-//     true,
-//     true,
-//     true,
-//     true,
-//     false,
-//     false,
-//     true,
-//     true,
-//   ])
-// );
+const num = [10, 2, 4, 6, 8];
 
-// console.log([
-//   true,
-//   true,
-//   true,
-//   false,
-//   true,
-//   true,
-//   true,
-//   true,
-//   true,
-//   false,
-//   true,
-//   false,
-//   true,
-//   false,
-//   false,
-//   true,
-//   true,
-//   true,
-//   true,
-//   true,
-//   false,
-//   false,
-//   true,
-//   true,
-// ]);
+const sum = num.reduce((t, n) => t + n, 0);
 
-
-console.clear();
-
-/* 
-Introduction
-The first century spans from the year 1 up to and including the year 100, the second century - from the year 101 up to and including the year 200, etc.
-
-Task
-Given a year, return the century it is in.
-
-EXAMPLE
-1705 --> 18 -- 1705 / 100 (17.05) jei nesveikas skaicius + 1,  Math.ceil(17.05)
-1900 --> 19 -- 1900 / 100 (19) 
-1601 --> 17
-2000 --> 20
-
-*/
-/*  console.clear()
-
-function century(year) {
-  if ((year / 100) % 1 === 0) {
-    return year / 100;
-  } else {
-    return Math.ceil(year / 100);
-  }
-
-  // return Math.ceil(year / 100);
-}
-
-
-
-
-console.log(century(1901)); */
-
-console.clear();
-
-/* 
-Your task is to create a function that does four basic mathematical operations.
-
-The function should take three arguments - operation(string/char), value1(number), value2(number).
-The function should return result of numbers after applying the chosen operation.
-
-('+', 4, 7) --> 11 | 4 + 7
-('-', 15, 18) --> -3 
-('*', 5, 5) --> 25
-('/', 49, 7) --> 7
-*/
-
-function basicOp(operation, value1, value2) {
-// if(operation === '+') {
-//     return value1 + value2;
-// }  
-// if(operation === '-') {
-//     return value1 - value2;
-// }  
-// if(operation === '*') {
-//     return value1 * value2;
-// }  
-// if(operation === '/') {
-//     return value1 / value2;
-// }  
-
-}
-
-// basicOp("/", 49, 7);
-
-// console.log(basicOp("-", 20, 2))
-
-    // const operation = 'labas';
-
-    // switch (operation) {
-    //   case "+":
-    //     console.log('+');
-    //   case "-":
-    //      console.log('-');
-    //   case "*":
-    //     console.log("/");
-    //     break;
-    //   case "/":
-    //     console.log("*");
-    //     default:
-    //         console.log('ERROR');
-    //         break;
-    // };
+console.log(sum);
